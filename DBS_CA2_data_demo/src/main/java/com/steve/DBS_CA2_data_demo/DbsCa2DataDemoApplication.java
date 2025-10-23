39e1a96ed96b1ef85f9aa4cf7f755bbd3760e107
Htun Thiha (Steve) <htunthiha.dev01@gmail.com>
2025-10-23 20:30:40 +0800

    (new) dbs_ca2_data_demo: Initial setup

---
+++ DBS_CA2_data_demo/src/main/java/com/steve/DBS_CA2_data_demo/DbsCa2DataDemoApplication.java
@@ -0,0 +1,13 @@
+package com.steve.DBS_CA2_data_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbsCa2DataDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbsCa2DataDemoApplication.class, args);
+	}
+
+}
---
+++ DBS_CA2_data_demo/src/test/java/com/steve/DBS_CA2_data_demo/DbsCa2DataDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steve.DBS_CA2_data_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbsCa2DataDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
